a877a0709657b34ab067c723b9266db188aca925
KhalidBinMeshal <41529068+KhalidBinMeshal@users.noreply.github.com>
2019-05-17 09:05:15 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,4 +11,22 @@ client.on('message', msg => {
   }
 });
 
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+
+client.on("message", msg => {
+    if(msg.content.startsWith("blocks")){
+        const embed = new Discord.RichEmbed()
+        blocksmc.player(msg.content.split(" ").slice(1)).then(player => {
+            player.map(g => {
+               
+                embed.addField(g.game, `    Kills: ${g.stats.Kills} \n      Deaths: ${g.stats.Deaths}`, true)
+                
+            });
+            msg.channel.send(embed)
+        }); 
+       
+    }
+});
+  
 client.login(process.env.BOT_TOKEN);
